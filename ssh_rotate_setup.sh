3af2002b720e143a15b8a2141976b0fd562baf5b
Þorgils Árni Hjalmarsson <45407193+creep1g@users.noreply.github.com>
2023-09-01 14:00:56 +0000

    Update ssh_rotate_setup.sh

--- ssh_rotate_setup.sh
+++ ssh_rotate_setup.sh
@@ -9,7 +9,7 @@ sed -i 's/#PermitUserEnvironment no/PermitUserEnvironment yes/g' /etc/ssh/sshd_c
 # Update LogLevel to Verbose
 sed '/^[^#]*LogLevel.*\(QUIET\|FATAL\|ERROR\|INFO\)/{s/^/# /;h;s/$/\nLogLevel VERBOSE/};${p;g;/./!{iLogLevel VERBOSE'$'\n;};D}'  -i /etc/ssh/sshd_config &&
 # Get the hook script
-curl -s 'https://raw.githubusercontent.com/creep1g/SSHrotate/main/ssh_rotate.sh?token=GHSAT0AAAAAACEWTTV3XJGCI6T37CO7JJYQZHIXJFQ' --output /etc/profile.d/ssh_rotate.sh &&
+curl -s 'https://raw.githubusercontent.com/creep1g/SSHrotate/main/ssh_rotate.sh' --output /etc/profile.d/ssh_rotate.sh &&
 chmod 0555 /etc/profile.d/ssh_rotate.sh
 # Restart SSH service
 /usr/bin/systemctl restart sshd &&
@@ -23,5 +23,5 @@ echo ':msg, regex, "Accepted publickey for" -/var/log/sshdusers.log' >> /etc/rsy
 touch /var/log/sshdusers.log &&
 chmod 644 /var/log/sshdusers.log
 # Get logrotate config
-curl -s 'https://raw.githubusercontent.com/creep1g/SSHrotate/main/sshdusers?token=GHSAT0AAAAAACEWTTV36TBBOD2I4OM4ONGUZHIYHEA' --output /etc/logrotate.d/sshdusers
+curl -s 'https://raw.githubusercontent.com/creep1g/SSHrotate/main/sshdusers' --output /etc/logrotate.d/sshdusers
 
